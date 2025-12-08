bb3635d639e289b62e1e1fed2920bb4ea2c61997
TheAJ88 <akshatjohanan@gmail.com>
2025-12-08 01:56:57 -0700

    W06 PONDER

--- W06 PONDER/arrays.js
+++ W06 PONDER/arrays.js
@@ -4,7 +4,7 @@ function listTemplate(step) {
   return `<li>${step}</li>`;
 }
 const stepsHtml = steps.map(listTemplate);
-document.querySelector("#myList").innerHTML = stepsHtml.join("");
+document.querySelector("#myList").innerHTML = stepsHtml.join();
 
 // Activity 2
 
