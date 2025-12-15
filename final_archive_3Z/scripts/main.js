a68970ec6c5a00aea4211019ae189b900addb848
TheAJ88 <akshatjohanan@gmail.com>
2025-12-15 16:37:23 -0600

    FINISHED FINAL PROJECT

--- final_archive_3Z/scripts/main.js
+++ final_archive_3Z/scripts/main.js
@@ -5,18 +5,18 @@ import { initStoryPreviews, initFullStoryLinks } from "./story-preview.js";
 
 
 (async () => {
-    await loadTemplate("header", "templates/header.html");
-    await loadTemplate("footer", "templates/footer.html");
+    // await loadTemplate("header", "templates/header.html");
+    // await loadTemplate("footer", "templates/footer.html");
 
-    // Adjust header links to be absolute
-    const rootPath = location.pathname.startsWith("/final_archive_3Z/") ? "/final_archive_3Z/" : "/";
-    const headerLinks = document.querySelectorAll("#header a");
-    headerLinks.forEach(a => {
-        const href = a.getAttribute("href");
-        if (href && !href.startsWith("http")) {
-            a.href = rootPath + href;
-        }
-    });
+    // // Adjust header links to be absolute
+    // const rootPath = location.pathname.startsWith("/final_archive_3Z/") ? "/final_archive_3Z/" : "/";
+    // const headerLinks = document.querySelectorAll("#header a");
+    // headerLinks.forEach(a => {
+    //     const href = a.getAttribute("href");
+    //     if (href && !href.startsWith("http")) {
+    //         a.href = rootPath + href;
+    //     }
+    // });
 
     initSearch();
     initThemeToggle();
